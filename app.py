3ff67c6fc485882fb5b23fafccbfc4839499b5c6
grantsut <grantsut@gmail.com>
2024-02-02 21:25:28 +0100

    made chat interface

---
+++ app.py
@@ -0,0 +1,46 @@
+import streamlit as st
+from openai import OpenAI
+
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+st.title("Forecast Friend")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = [
+        {"role": "system", "content": "You are a helpful assistant."}
+    ]
+
+
+def generate_response(input_text):
+    response = client.generate_text(input_text)
+    return response
+
+
+# Display chat history
+for message in st.session_state.messages[1:]:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+
+# Add user input from text box
+if prompt := st.text_input(""):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Generate response using full chat history
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in client.chat.completions.create(
+            temperature=0.9,
+            model="gpt-3.5-turbo",
+            messages=st.session_state.messages,
+            stream=True,
+        ):
+            full_response += response.choices[0].delta.content or ""
+            message_placeholder.markdown(full_response)
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
