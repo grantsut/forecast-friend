c2d957b5b7f02e3273cb4e064a3b934fba66e629
grantsut <grantsut@gmail.com>
2024-02-05 19:38:13 +0100

    initial pytest tests

--- app.py
+++ app.py
@@ -17,7 +17,6 @@ for message in st.session_state.messages[1:]:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-
 if prompt := st.chat_input("How can I help?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
