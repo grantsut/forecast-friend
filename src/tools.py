98e724791f7183ce991c5f66753963e93eb4c160
grantsut <grantsut@gmail.com>
2024-02-16 16:19:52 +0100

    removed junk code

--- src/tools.py
+++ src/tools.py
@@ -2,16 +2,21 @@ import os
 import json
 import requests
 
-def fred_series_search(search_text: str, frequency: str | None = None, return_fields: list[str] | None = None) -> str:
+
+def fred_series_search(
+    search_text: str,
+    frequency: str | None = None,
+    return_fields: list[str] | None = None,
+) -> str:
     """
     Submits a query to the FRED search API and returns results as a serialized JSON.
 
     Args:
         search_text (str): space-separated terms to search for in series titles, units, frequencies and tags.
         frequency (str, optional): Optional frequency filter of the data, e.g. "Monthly", "Annual", "Daily".
-        return_fields (list[str], optional): Optional additional fields to return in addition to the series title, ID 
-        frequency and last_updated. Options are: 'realtime_start', 'realtime_end', 'observation_start', 'observation_end', 
-        'frequency_short', 'units', 'units_short', 'seasonal_adjustment', 'seasonal_adjustment_short', 'popularity', 
+        return_fields (list[str], optional): Optional additional fields to return in addition to the series title, ID
+        frequency and last_updated. Options are: 'realtime_start', 'realtime_end', 'observation_start', 'observation_end',
+        'frequency_short', 'units', 'units_short', 'seasonal_adjustment', 'seasonal_adjustment_short', 'popularity',
         'group_popularity', 'notes'.
     """
     params = {
@@ -50,7 +55,3 @@ def fred_series_search(search_text: str, frequency: str | None = None, return_fi
             return json.dumps(results)
     else:
         return f"Call to API failed with status code {r.status_code}."
-
-
-j = fred_series_search("wheat", frequency="Monthly")
-j
